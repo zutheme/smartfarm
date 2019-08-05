84519d461e86033186459380beb2ee6ddb9afedc
hatazu <hatazu@gmail.com>
2019-08-05 11:20:03 +0700

    first project

---
+++ resources/views/teamilk/footer.blade.php
@@ -0,0 +1,105 @@
+<!--BEGIN: LAYOUT/FOOTERS/FOOTER-5 -->
+<a name="footer"></a>
+<footer class="c-layout-footer c-layout-footer-3 c-bg-dark">
+  <div class="c-prefooter">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4">
+          <div class="c-container c-first">
+            <div class="c-content-title-1">
+              <h3 class="c-font-uppercase c-font-bold c-font-white"><img src="{{ asset('assets-tea/images/logo/footer-smart-farm.png') }}"></h3>
+              <div class="c-line-left hide"></div>
+              <p class="c-text">
+              smartfarm sẽ là bước tiên phong ứng dụng công nghệ thông tin giúp nông dân sản xuất thông minh và hiểu quả trong việc canh tác khoa học và ổn định đầu ra sản phẩm.</p>
+              {{-- <p>Hỗ trợ (24/7): 19001768</p> --}}
+            </div>
+            <ul class="c-links">
+              <li><a href="{{ url('/') }}">Trang chủ</a></li>
+              <li><a href="{{ url('/') }}">Giới thiệu</a></li>
+              <li><a href="{{ url('/') }}">Chính sách</a></li>
+              <li><a href="{{ url('/') }}">Liên hệ</a></li>
+            </ul>
+            
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="c-container">
+            <div class="c-content-title-1">
+              <h3 class="c-font-uppercase c-font-bold c-font-white">Liên hệ</h3>
+              <div class="c-line-left hide"></div>
+            </div>
+            <div class="c-blog">
+              <div class="c-post">
+                <div class="c-post-content">
+                  <h4 class="c-post-title"><a href="#">CN TP. Hồ Chí Minh</a></h4>
+                  <p class="c-text"><i class="fa fa-home" aria-hidden="true"></i>&nbsp;Số 7, Trần Quang Diệu, P.14, Quận 3, TpHCM</p>
+                  <p class="c-text"><a href="tel:02839318831"><i class="fa fa-phone" aria-hidden="true"></i>&nbsp;(028) 3931 8831</a>
+                
+                </p>       
+                </div>
+              </div>
+              <div class="c-post">
+                <div class="c-post-content">
+                  <h4 class="c-post-title"><a href="#">CN Bình dương</a></h4>
+                  <p class="c-text"><i class="fa fa-home" aria-hidden="true"></i>&nbsp;Số 83, Đường 30/4, P.Phú Hòa, Tp.Thủ Dầu Một</p>
+                <p class="c-text"><a href="tel:02746230088"><i class="fa fa-phone" aria-hidden="true"></i>&nbsp;(027) 4623 0088</a></div>
+              </div>
+              <div class="c-post c-last">
+                <div class="c-post-content">
+                  <h4 class="c-post-title"><a href="#">CN Đồng Nai</a></h4>
+                  <p class="c-text"><i class="fa fa-home" aria-hidden="true"></i>&nbsp;D2-D3/253 Phạm Văn Thuận, P.Tân Mai, Tp.Biên Hòa</p>
+                  <p class="c-text"><a href="tel:02516569638"><i class="fa fa-phone" aria-hidden="true"></i>&nbsp;(025) 1656 9638</a></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        
+        <div class="col-md-4 communication">
+          <div class="c-container c-last">
+            <div class="c-content-title-1">
+              <h3 class="c-font-uppercase c-font-bold c-font-white">Cộng đồng</h3>
+              <div class="c-line-left hide"></div>
+              {{-- <div class="fb-page" data-href="https://www.facebook.com/thammyvienthienkhue/" data-width="390" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/thammyvienthienkhue/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/thammyvienthienkhue/">Hệ Thống Thẩm Mỹ Quốc Tế Thiên Khuê</a></blockquote></div> --}}
+              <p>smartfarm sẽ là bước tiên phong ứng dụng công nghệ thông tin giúp nông dân sản xuất thông minh và hiểu quả trong việc canh tác khoa học và ổn định đầu ra sản phẩm.</p>
+            </div>
+            <div class="c-line-left hide"></div>
+            <div class="contact">
+             <ul class="c-socials">
+              <li><a href="#"><i class="icon-social-facebook"></i></a></li>
+              <li><a href="#"><i class="icon-social-twitter"></i></a></li>
+              <li><a href="#"><i class="icon-social-youtube"></i></a></li>
+              <li><a href="#"><i class="icon-social-tumblr"></i></a></li>
+            </ul>
+            <ul class="c-address">
+              <li><i class="icon-pointer c-theme-font"></i> Số 7, Trần Quang Diệu, Quận 3, TP. Hồ Chí Minh</li>
+              <li><i class="icon-call-end c-theme-font"></i> 1900 1768</li>
+              <li><i class="icon-envelope c-theme-font"></i> thammyvienthienkhue@gmail.com</li>
+            </ul>
+          </div>
+          </div>
+        </div>
+      </div>    
+    </div>
+  </div>
+  <div class="c-postfooter">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 col-sm-12 c-col">
+          <p class="c-copyright c-font-grey">2019 &copy; mgkgroup
+            <span class="c-font-grey-3">All Rights Reserved.</span>
+          </p>
+        </div>
+                  <div class="col-md-6 col-sm-12 c-col">
+            <ul class="list-unstyled list-inline pull-right">
+              <li><img class="img-responsive" src="{{ asset('assets-tea/assets/base/img/content/payments/light/1.png') }}" width="60"></li>
+              <li><img class="img-responsive" src="{{ asset('assets-tea/assets/base/img/content/payments/light/2.png') }}" width="60"></li>
+              <li><img class="img-responsive" src="{{ asset('assets-tea/assets/base/img/content/payments/light/22.png') }}" width="60"></li>
+              <li><img class="img-responsive" src="{{ asset('assets-tea/assets/base/img/content/payments/light/5.png') }}" width="60"></li>
+              <li><img class="img-responsive" src="{{ asset('assets-tea/assets/base/img/content/payments/light/6.png') }}" width="60"></li>
+            </ul>
+          </div>
+      </div>
+    </div>
+  </div>
+</footer><!-- END: LAYOUT/FOOTERS/FOOTER-5
